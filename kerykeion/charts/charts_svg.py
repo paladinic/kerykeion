46cd92154e76f9d1069d852d37e384663cf2d56d
Claudio Paladini <clode.paladini@gmail.com>
2022-05-24 06:00:29 +0100

    Update charts_svg.py

--- kerykeion/charts/charts_svg.py
+++ kerykeion/charts/charts_svg.py
@@ -1558,7 +1558,7 @@ class MakeSvgInstance:
 
         return template.replace("\"", "'")
 
-    def makeSVG(self):
+    def makeSVG(self,path):
         """Prints out the SVG file in the specifide folder"""
 
         if not (self.template):
@@ -1567,10 +1567,10 @@ class MakeSvgInstance:
         self.chartname = self.output_directory / \
             f'{self.name}{self.chart_type}Chart.svg'
 
-        with open(self.chartname, "w", encoding='utf-8', errors='ignore') as output_file:
+        with open(path, "w", encoding='utf-8', errors='ignore') as output_file:
             output_file.write(self.template)
 
-        return print(f"SVG Generated Correctly in: {self.chartname}")
+        return print(f"SVG Generated Correctly in: {path}")
 
 
 if __name__ == "__main__":
